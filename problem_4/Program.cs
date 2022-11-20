6a3a55f824c5add9d3c41b2991dbc00a7dcf69ed
Elena <elenka81086@mail.ru>
2022-11-20 14:04:51 +0300

    задача 4

---
+++ problem_4/Program.cs
@@ -0,0 +1,12 @@
+﻿/* напишите программу, которая на вход принимает 
+число N и выдает целые число от -N до N*/
+int Number = new int();
+Console.WriteLine("Введите число");
+Number = Convert.ToInt32(Console.ReadLine());
+int A = - Number;
+
+while (A <= Number)
+{
+    Console.Write(A + " ");
+    A = A + 1;
+}
